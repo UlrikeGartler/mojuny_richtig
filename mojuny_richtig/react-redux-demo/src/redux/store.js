a949a90ea897f9133bf20df0f41032bfb688fd6b
Ulrike Gartler <ulrike.gartler@AXTESYS.local>
2021-01-18 19:49:54 +0100

    prior status

--- mojuny_richtig/react-redux-demo/src/redux/store.js
+++ mojuny_richtig/react-redux-demo/src/redux/store.js
@@ -1,7 +1,11 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import mainCategoryReducer from "./reducers/mainCategoryReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 
-const store = createStore(mainCategoryReducer, composeWithDevTools(applyMiddleware()));
+const store = createStore(
+  mainCategoryReducer,
+  composeWithDevTools(compose(applyMiddleware(thunk.withExtraArgument({}))))
+);
 
 export default store;
--- mojuny_richtig/react-redux-demo/src/screens/SupportScreen.jsx
+++ mojuny_richtig/react-redux-demo/src/screens/SupportScreen.jsx
@@ -1,15 +1,12 @@
-
 import React from "react";
 import { BrowserRouter, Route, Switch, Link, Redirect } from "react-router-dom";
 
 export function SupportScreen(props) {
   return (
     <div className="screen">
-    
-        <Link to="/mainCategory">
-          <button>next</button>
-        </Link>
-      
+      <Link to="/mainCategory">
+        <button>next</button>
+      </Link>
     </div>
   );
 }
