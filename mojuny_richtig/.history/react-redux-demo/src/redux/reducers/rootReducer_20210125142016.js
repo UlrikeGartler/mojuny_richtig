858678c304f812a62d9452226e53f5698a7098d3
Ulrike Gartler <ulrike.gartler@AXTESYS.local>
2021-01-25 20:41:24 +0100

    add Readme

--- mojuny_richtig/.history/react-redux-demo/src/redux/reducers/rootReducer_20210125142016.js
+++ mojuny_richtig/.history/react-redux-demo/src/redux/reducers/rootReducer_20210125142016.js
@@ -4,6 +4,8 @@ import subCategoryDecisionReducer from "./subCategoryReducer";
 import solutionDecisionReducer from "./solutionReducer";
 
 const rootReducer = combineReducers({
+  // firebase: firebaseReducer,
+  // firestore: firestoreReducer,
   main: mainCategoryDecisionReducer, // put in correct thing
   sub: subCategoryDecisionReducer,
   sol: solutionDecisionReducer,
--- mojuny_richtig/react-redux-demo/src/redux/index.js
+++
@@ -1,3 +0,0 @@
-export { takeStrategy, takeMotivation } from "./actions/mainCategoryActions";
-export { clickOnASK, clickOnCODE } from "./actions/subCategoryActions";
-export { takeSolution1, takeSolution2 } from "./actions/solutionTypes";
